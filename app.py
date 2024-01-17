95c0a2256f5987de40ed85a2d28a7278adbc2605
Mattias Lindhe <mattias.lindhe@gmail.com>
2024-01-17 16:14:04 +0100

    Add tasks GET and POST endpoint
    Added GET and POST for tasks

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 from flask import Flask, request, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello world"
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    with open("tasks.json", "r") as file:
+        data = json.load(file)
+    return data
+    
+@app.route("/tasks", methods=["POST"])
+def add_tasks():
+    with open("tasks.json", "w") as file:
+        json.dump(request.form, file, indent=4)
+    return "Task was added"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
